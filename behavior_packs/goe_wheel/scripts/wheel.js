5e63568bf2b03cec02101cbcd364e3d14214798d
krupanjac <arsen.djurdjev@live.com>
2025-11-25 14:49:50 +0100

    first commit

---
+++ behavior_packs/goe_wheel/scripts/wheel.js
@@ -0,0 +1,35 @@
+import { world } from "@minecraft/server";
+
+world.afterEvents.dataDrivenEntityTrigger.subscribe(ev => {
+    if (ev.id !== "goe:give_reward") return;
+
+    const player = ev.entity;
+    if (!player || player.typeId !== "minecraft:player") return;
+
+    const wheel = [...world.getEntities({
+        type: "goe:wheel_of_fortune",
+        location: player.location,
+        maxDistance: 6
+    })][0];
+    if (!wheel) return;
+
+    // Check rotation on Z axis (roll) since animation is on Z
+    const rot = wheel.getRotation().z % 360;
+
+    let reward;
+
+    // Normalize rotation to positive
+    let normalizedRot = rot;
+    if (normalizedRot < 0) normalizedRot += 360;
+
+    if (normalizedRot < 60) reward = "emerald 3";
+    else if (normalizedRot < 120) reward = "diamond 1";
+    else if (normalizedRot < 180) reward = "iron_ingot 10";
+    else if (normalizedRot < 240) reward = "gold_ingot 5";
+    else if (normalizedRot < 300) reward = "cookie 5";
+    else reward = "netherite_scrap 1";
+
+    player.runCommandAsync(`give @s ${reward}`);
+    player.runCommandAsync(`playsound random.levelup @s`);
+    player.runCommandAsync(`particle minecraft:happy_villager ~~~`);
+});
